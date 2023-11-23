e9d609248648335a653e8aa5e07130ab798abb3e
FieRZe <vbgbiz@mail.ru>
2023-11-23 12:46:06 +0300

    fix(skills): start time of filling progress circle
    круг прогресса появляется уже заполненным нужно начало анимации
    активировать после появления круга прогресса.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,39 @@
 // Текущий style.css на странице в который будем добавлять стили:
 const style = window.document.styleSheets[0];
 
+
+
+/* Появление элементов при скроллинге */
+//#region 
+
+const options = {
+    //rootMargin: "0px",
+    threshold: [ 0.5 ]
+};
+
+// объявляем callback-функцию, которая будет срабатывать при пересечении
+function onIntersectionFadeIn(entries) {
+    entries.forEach((entry) => {
+        if(entry.isIntersecting) {
+            entry.target.classList.add("fade-in");
+        } 
+    })
+}
+
+// создаём observer
+const observerFadein = new IntersectionObserver(onIntersectionFadeIn, options)
+
+// получаем элементы, за которыми наблюдаем
+const fadeInElements = document.querySelectorAll(".show-on-scroll");
+// подписываем элементы на observerFadein
+fadeInElements.forEach((e) => {observerFadein.observe(e)});
+
+//#endregion
+
+
+
+
+
 /* Увеличение процентов от нуля до указанного числа */
 //#region
 
@@ -14,65 +47,78 @@ const strokeDashOffsetDefault = 230;
 // Длина ID префикса, для substring, чтобы получить имя навыка
 const skillIDPrefixLength = skillIDPrefix.length;
 
-// Собираем все элементы с нашим ID префиксом
-const skills = document.querySelectorAll(`[id^=${skillIDPrefix}]`);
-
-// В каждом навыке
-skills.forEach(skill => {
-    // получаем уникальный class-префикс с именем навыка
-    const skillClassPrefix = skill.id.substring(skillIDPrefixLength);
-
-    // из html конвертируем установленный процент прогресса навыка string в int
-    const skillPercentsHtml = Number(skill.querySelector(".progress-number").textContent);
-
-    // пересчёт html число в stroke-dashoffset для анимации
-    const skillStrokeDashOffset = strokeDashOffsetDefault - (strokeDashOffsetDefault / 100 * skillPercentsHtml);
-    
-    // подготовка css стиля для добавления анимации с уникальным именем
-    const skillClassRule = `.${skillClassPrefix} {
-        animation: anim-${skillClassPrefix} 2s linear forwards 2s;
-    }`
-
-    // подготовка css keyframes с уникальным именем
-    const keyframesRule = `@keyframes anim-${skillClassPrefix} {
-        100% {
-            stroke-dashoffset: ${skillStrokeDashOffset};
-        }
-    }`
-
-    // Добавить правила в текущий style.css
-    style.insertRule(skillClassRule);
-    style.insertRule(keyframesRule);
-});
-
-//#endregion
-
-/* Появление элементов при скроллинге */
-//#region 
-
-const options = {
-    //rootMargin: "0px",
-    threshold: [ 0.5 ]
-};
-
-// объявляем callback-функцию, которая будет срабатывать при пересечении
-function onIntersection(entries) {
-    entries.forEach((entry) => {
+// Объявляем callback-функцию, которая будет срабатывать при пересечении
+const onIntersectionFillProgress = (skillsEntries) => {
+    skillsEntries.forEach((entry) => {
+        // В каждом навыке, который попадает в область видимости
         if(entry.isIntersecting) {
-            entry.target.classList.add("fade-in");
-        } 
-    })
+            console.log(entry.target);
+
+            // получаем уникальный class-префикс с именем навыка
+            const skillClassPrefix = entry.target.id.substring(skillIDPrefixLength);
+
+            // из html конвертируем установленный процент прогресса навыка string в int
+            const skillPercentsHtml = Number(entry.target.querySelector(".progress-number").textContent);
+
+            // пересчёт html число в stroke-dashoffset для анимации
+            const skillStrokeDashOffset = strokeDashOffsetDefault - (strokeDashOffsetDefault / 100 * skillPercentsHtml);
+            
+            // подготовка css стиля для добавления анимации с уникальным именем
+            const skillClassRule = `.${skillClassPrefix} {
+                animation: anim-${skillClassPrefix} 2s linear forwards;
+            }`
+
+            // подготовка css keyframes с уникальным именем
+            const keyframesRule = `@keyframes anim-${skillClassPrefix} {
+                100% {
+                    stroke-dashoffset: ${skillStrokeDashOffset};
+                }
+            }`
+
+            // Добавить правила в текущий style.css
+            style.insertRule(skillClassRule);
+            style.insertRule(keyframesRule);
+            }
+        })
+
 }
 
 // создаём observer
-const observer = new IntersectionObserver(onIntersection, options)
+const observerSkills = new IntersectionObserver(onIntersectionFillProgress, options);
 
-// получаем элементы, за которыми наблюдаем
-const fadeInElements = document.querySelectorAll(".invisible");
-// подписываем элементы на observer
-fadeInElements.forEach((e) => {observer.observe(e)});
+// Собираем все элементы с нашим ID префиксом, за которыми наблюдаем
+const skills = document.querySelectorAll(`[id^=${skillIDPrefix}]`);
 
+// подписываем элементы на observerSkills
+skills.forEach((s) => {observerSkills.observe(s)});
+// // В каждом навыке
+// skills.forEach(skill => {
+//     // получаем уникальный class-префикс с именем навыка
+//     const skillClassPrefix = skill.id.substring(skillIDPrefixLength);
 
+//     // из html конвертируем установленный процент прогресса навыка string в int
+//     const skillPercentsHtml = Number(skill.querySelector(".progress-number").textContent);
+
+//     // пересчёт html число в stroke-dashoffset для анимации
+//     const skillStrokeDashOffset = strokeDashOffsetDefault - (strokeDashOffsetDefault / 100 * skillPercentsHtml);
+    
+//     // подготовка css стиля для добавления анимации с уникальным именем
+//     const skillClassRule = `.${skillClassPrefix} {
+//         animation: anim-${skillClassPrefix} 2s linear forwards 2s;
+//     }`
+
+//     // подготовка css keyframes с уникальным именем
+//     const keyframesRule = `@keyframes anim-${skillClassPrefix} {
+//         100% {
+//             stroke-dashoffset: ${skillStrokeDashOffset};
+//         }
+//     }`
+
+//     // Добавить правила в текущий style.css
+//     style.insertRule(skillClassRule);
+//     style.insertRule(keyframesRule);
+// });
 
 //#endregion
+
 
